c031ef9e121c4cd3be5721ac8896a2534c211aa7
Lukasz Strzelecki <lu.strzelecki@gmial.com>
2012-05-27 18:46:44 +0200

    add copy right headers

--- adapters/ormlite/src/main/java/com/fizzner/valueobject/time/adapter/ormlite/DayType.java
+++ adapters/ormlite/src/main/java/com/fizzner/valueobject/time/adapter/ormlite/DayType.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fizzner.valueobject.time.adapter.ormlite;
 
 import com.fizzner.valueobject.time.Day;
@@ -30,7 +46,7 @@ public class DayType extends BaseDateType {
 			return Long.parseLong(defaultStr);
 		} catch (NumberFormatException e) {
 			throw SqlExceptionUtil.create("Problems with field " + fieldType + " parsing default date-long value: "
-					+ defaultStr, e);
+										  + defaultStr, e);
 		}
 	}
 
@@ -41,7 +57,7 @@ public class DayType extends BaseDateType {
 
 	@Override
 	public Object sqlArgToJava(FieldType fieldType, Object sqlArg, int columnPos) {
-		return Day.fromMillis((Long)sqlArg);
+		return Day.fromMillis((Long) sqlArg);
 	}
 
 	@Override
